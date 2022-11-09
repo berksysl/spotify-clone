7db0c1abbd37d1aece77442d3e4268d301429e93
Tahaberk <berksysl.g@gmail.com>
2022-11-09 13:56:38 +0300

    Spotify Responsive Navigation Bar V0.0

--- spotify-nav-bar/app.js
+++ spotify-nav-bar/app.js
@@ -1,6 +1,9 @@
 const navSection = document.querySelector('.hamburger-lines');
 const mobileNav = document.querySelector('.mobile-nav');
+const logo = document.querySelector('.logo');
+
 navSection.addEventListener('click', () => {
     navSection.classList.toggle("open");
     mobileNav.classList.toggle("open");
+    logo.classList.toggle("open");
 });
