de936b18bb3677439c872e2de285584fe4bb8585
Tahaberk <berksysl.g@gmail.com>
2022-11-13 14:18:18 +0300

    Modify

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const navSection = document.querySelector('.hamburger-lines');
 const mobileNav = document.querySelector('.mobile-nav');
 const logo = document.querySelector('.logo');
+const body = document.querySelector('.body');
 
 navSection.addEventListener('click', () => {
     navSection.classList.toggle("open");
     mobileNav.classList.toggle("open");
     logo.classList.toggle("open");
+    body.classList.toggle("open");
 });
